6980f5bbf9411971bc292b18dd39fa4f57ce27b1
Ikedinobi Onyeka <ikedinobicruz7@gmail.com>
2023-06-04 23:58:59 +0100

    more sections added

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,10 @@ import heroImg3 from "@/images/hero-image-03.svg"
 import { BsChatSquareText } from "react-icons/bs"
 import { AnimatedText } from "@/components/AnitmatedText";
 import { Variants, motion } from "framer-motion";
+import servicesImg1 from "@/images/my-service-image-01.svg"
+import servicesImg2 from "@/images/my-service-image-02.svg"
+import gridImg from "@/images/grid-image.svg"
+import aboutmeImg from "@/images/about-me-image.svg"
 
 export default function Home() {
 
@@ -27,8 +31,31 @@ export default function Home() {
         }
     }
 
+    const services = [
+        {
+            name: "Frontend Development",
+            description: `
+                I specialize in creating visually captivating and user-friendly websites,
+                blending creativity with functionality as a frontend developer.
+            `
+        },
+        {
+            name: "Backend Engineering",
+            description: `
+                I deliver robust and efficient solutions that power the core functionalities of your applications, 
+                ensuring seamless performance and scalability.
+            `
+        },
+        {
+            name: "Software Development",
+            description: `
+                Tailored software development services, satisfying exclusive business needs with exceptional solutions.
+            `
+        }
+    ]
+
     return <>
-        <main className="min-h-screen relative">
+        <main className="min-h-screen relative overflow-hidden">
             <Navbar />
             <div className="hidden -z-10 sm:block absolute top-0 right-0">
                 <Image
@@ -38,7 +65,7 @@ export default function Home() {
                 />
             </div>
             <div className="my-container sm:my-10 md:mb-14 gap-[4%] flex flex-wrap sm:h-[65vh] md:h-[80vh] min-[1300px]:h-[65vh]">
-                <motion.div 
+                <motion.div
                     className="relative w-full h-auto sm:w-[43%]"
                     variants={appear}
                     initial="hidden"
@@ -73,25 +100,95 @@ export default function Home() {
                         </div>
                         <p className="font-black">Chat with me</p>
                     </div>
-                    <AnimatedText 
-                        text="Your Design & Development Partner"  
+                    <AnimatedText
+                        text="Your Design & Development Partner"
                         classname="text-4xl leading-[1.4] sm:text-5xl min-[980px]:text-6xl lg:text-7xl sm:leading-[1.2] font-black"
                     />
-                    <button className="w-fit py-[10px] px-[60px] bg-white rounded-tr-[40px] text-sm font-extrabold text-black">
+                    <button className="my-button">
                         Lets talk
                     </button>
                 </div>
             </div>
-            <div className="my-container my-10">
-                <p className="text-2xl leading-[1.4] sm:text-5xl font-black mb-6 sm:mb-10">My Services</p>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-                    <div className="p-4 bg-white">
-
+            <div className="my-container my-32 sm:my-40">
+                <p className="text-2xl leading-[1.4] sm:text-5xl font-black sm:mb-8">Onyeka Ikedinobi</p>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                    <div className="sm:order-2">
+                        <Image 
+                            src={aboutmeImg}
+                            alt=""
+                            className="w-full my-5 sm:my-0 sm:h-[400px] object-contain"
+                        />
                     </div>
-                    <div className="p-4 bg-white hidden sm:block">
-
+                    <div className="flex flex-col justify-center gap-4">
+                        <p>
+                            Hey, my name is  and I am across social medias. I’m a graphic designer, 
+                            UX/UI designer & front-end web developer from France. 
+                            I’m also passionate about pop music and make portraits and universes around what I listen to and 
+                            I’m always curious to learn more when it comes to new technologies and creative coding.
+                        </p>
+                        <button className="my-button">
+                            Resume
+                        </button>
                     </div>
                 </div>
+            </div>
+            <div className="my-container relative my-32 sm:my-40">
+                <p className="text-2xl leading-[1.4] sm:text-5xl font-black sm:mb-8">My Services</p>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                    <div className="p-4 grid grid-cols-1 divide-y divide-zinc-500">
+                        {
+                            services.map(({ name, description }, index) => (
+                                <div className="flex gap-4 py-5" key={index}>
+                                    <div className="w-1/5 sm:w-1/4">
+                                        <p className=" font-black">0{index}</p>
+                                    </div>
+                                    <div className="w-4/5 sm:w-3/4">
+                                        <p className="text-xl font-bold">{name}</p>
+                                        <p className="text-slate-200">
+                                            {description}
+                                        </p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <div className="p-4 hidden justify-center items-center sm:flex">
+                        <div className="w-[80%] relative">
+                            <Image 
+                                src={servicesImg1}
+                                alt=""
+                                className="w-full h-auto "
+                            />
+                            <motion.div 
+                                className="w-full h-auto absolute top-0 left-0"
+                                initial={{
+                                    x: "-10%",
+                                    y: "-10%"
+                                }}
+                                whileHover={{
+                                    x: 0,
+                                    y: 0,
+                                }}
+                            >
+                                <Image 
+                                    src={servicesImg2}
+                                    alt=""
+                                    className="w-full"
+                                />
+                            </motion.div>
+                        </div>
+                    </div>
+                </div>
+                <Image 
+                    src={gridImg}
+                    alt=""
+                    className="object-contain absolute top-[-20%] left-[-100px] -z-10 w-[250px]"
+                />
+                <Image 
+                    src={gridImg}
+                    alt=""
+                    className="object-contain absolute bottom-[-20%] right-[-100px] -z-10 w-[250px] rotate-90"
+                />
             </div>
         </main>
     </>
