9be45d59bd03af26a45758d0d18a1d477b446e3b
Ikedinobi Onyeka <ikedinobicruz7@gmail.com>
2023-06-16 09:08:44 +0100

    seo meta-tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,30 @@
 
 import { MyCursor } from '@/components/AnimatedCursor/AnimatedCursor'
 import './globals.css'
-import { Marcellus, Mina } from 'next/font/google'
+import { Marcellus } from 'next/font/google'
+import Head from 'next/head'
 
-const mina = Mina({ weight: "700", subsets: ['latin'] })
 const marcellus = Marcellus({ weight: "400", subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Onyeka Ikedinobi',
-  description: 'Portifolio of Ikedinobi Onyeka',
+  title: 'Onyeka Ikedinobi - Software Engineer',
+  description: 'Explore the portfolio of Onyeka Ikedinobi, a skilled full-stack software engineer. Witness impressive projects and skills. Connect for collaborations and exciting software opportunities.',
+  openGraph: {
+    title: "Onyeka Ikedinobi - Software Engineer",
+    description: 'Explore the portfolio of Onyeka Ikedinobi, a skilled full-stack software engineer. Witness impressive projects and skills. Connect for collaborations and exciting software opportunities.',
+    site_name: "Onyeka Ikedinobi",
+    type: "website",
+    url: "https://ikecruz.dev",
+    locale: "en_US",
+  },
+  twitter: {
+    title: "Onyeka Ikedinobi - Software Engineer",
+    description: 'Explore the portfolio of Onyeka Ikedinobi, a skilled full-stack software engineer. Witness impressive projects and skills. Connect for collaborations and exciting software opportunities.',
+    site: "Onyeka Ikedinobi",
+    card: "summary_large_image",
+    creator: "@ikxcrxz",
+    url: "https://ikecruz.dev",
+  }
 }
 
 export default function RootLayout({
